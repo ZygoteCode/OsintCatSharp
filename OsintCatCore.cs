cc195446c6807fa998f1cae1f4d78f20497a6a0b
ZygoteCode <exilephotographer@proton.me>
2025-10-30 23:05:11 +0100

    First commit

---
+++ OsintCatCore.cs
@@ -0,0 +1,103 @@
+﻿using System.Text;
+using System.Text.Encodings.Web;
+
+namespace OsintCatSharp
+{
+    public class OsintCatCore
+    {
+        private HttpClient _httpClient;
+        private string _baseUri, _osintCatApiKey;
+
+        public OsintCatCore(string osintCatApiKey)
+        {
+            _osintCatApiKey = osintCatApiKey;
+            _baseUri = "https://osintcat.net/api";
+            _httpClient = OsintCatUtils.CreateHttpClient();
+        }
+
+        private string GetRequest(string url)
+        {
+            HttpResponseMessage response = _httpClient.GetAsync($"{_baseUri}{url}&id={_osintCatApiKey}").GetAwaiter().GetResult();
+
+            string body = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+            response.Dispose();
+
+            return body;
+        }
+
+        private string Query(string queryName, string queryString)
+        {
+            return GetRequest($"/{queryName}?query={Uri.EscapeDataString(queryString)}");
+        }
+
+        public string BreachLookup(string query)
+        {
+            return Query("breach", query);
+        }
+
+        public string DiscordLookup(string query)
+        {
+            return Query("discord", query);
+        }
+
+        public string RedditLookup(string query)
+        {
+            return Query("reddit", query);
+        }
+
+        public string DiscordToRoblox(string query)
+        {
+            return Query("discord-to-roblox", query);
+        }
+
+        public string EmailOSINT(string query)
+        {
+            return Query("email-osint", query);
+        }
+
+        public string PhoneOSINT(string query)
+        {
+            return Query("phone-osint", query);
+        }
+
+        public string Domain(string query)
+        {
+            return Query("domain", query);
+        }
+
+        public string GitHubOSINT(string query)
+        {
+            return Query("github-osint", query);
+        }
+
+        public string DiscordMonitor(string query)
+        {
+            return Query("discord-stalker", query);
+        }
+
+        public string IpInfo(string query)
+        {
+            return Query("ip", query);
+        }
+
+        public string DnsResolver(string query)
+        {
+            return Query("dns-resolver", query);
+        }
+
+        public string Username(string query)
+        {
+            return Query("username", query);
+        }
+
+        public string ChileanName(string query)
+        {
+            return Query("chilean-name", query);
+        }
+
+        public string Minecraft(string query)
+        {
+            return Query("minecraft", query);
+        }
+    }
+}
---
+++ OsintCatUtils.cs
@@ -0,0 +1,47 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace OsintCatSharp
+{
+    internal class OsintCatUtils
+    {
+        public static HttpClient CreateHttpClient()
+        {
+            SocketsHttpHandler handler = new SocketsHttpHandler
+            {
+                ConnectCallback = async (ctx, ct) =>
+                {
+                    Socket socket = new Socket(AddressFamily.InterNetworkV6, SocketType.Stream, ProtocolType.Tcp)
+                    {
+                        NoDelay = true,
+                        DualMode = true,
+                    };
+
+                    await socket.ConnectAsync(ctx.DnsEndPoint, ct).ConfigureAwait(false);
+                    return new NetworkStream(socket, ownsSocket: true);
+                },
+
+                UseProxy = false,
+                Proxy = null,
+                UseCookies = false,
+                AllowAutoRedirect = true,
+                AutomaticDecompression = DecompressionMethods.None,
+
+                PooledConnectionIdleTimeout = TimeSpan.FromMinutes(10),
+                PooledConnectionLifetime = Timeout.InfiniteTimeSpan,
+                MaxConnectionsPerServer = int.MaxValue,
+
+                EnableMultipleHttp2Connections = true
+            };
+
+            HttpClient client = new HttpClient(handler, disposeHandler: true)
+            {
+                Timeout = TimeSpan.FromSeconds(99999),
+                DefaultRequestVersion = HttpVersion.Version30,
+                DefaultVersionPolicy = HttpVersionPolicy.RequestVersionOrHigher
+            };
+
+            return client;
+        }
+    }
+}
